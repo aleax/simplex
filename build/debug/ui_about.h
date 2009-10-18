fab2c88acc1ec6037b8b2f5c2ea74d2251e1abd4
Alexey Bondarchuk <me@aleax.org.ua>
2009-10-18 19:31:29 +0300

    chenge about

--- build/debug/ui_about.h
+++ build/debug/ui_about.h
@@ -1,7 +1,7 @@
 /********************************************************************************
 ** Form generated from reading ui file 'about.ui'
 **
-** Created: Sun Oct 18 18:15:20 2009
+** Created: Sun Oct 18 19:28:19 2009
 **      by: Qt User Interface Compiler version 4.5.3
 **
 ** WARNING! All changes made in this file will be lost when recompiling ui file!
@@ -15,8 +15,10 @@
 #include <QtGui/QApplication>
 #include <QtGui/QButtonGroup>
 #include <QtGui/QFrame>
+#include <QtGui/QGridLayout>
 #include <QtGui/QHeaderView>
 #include <QtGui/QPushButton>
+#include <QtGui/QSpacerItem>
 #include <QtGui/QTextBrowser>
 
 QT_BEGIN_NAMESPACE
@@ -24,27 +26,44 @@ QT_BEGIN_NAMESPACE
 class Ui_About
 {
 public:
-    QTextBrowser *textBrowser;
+    QFrame *gridFrame;
+    QGridLayout *gridLayout;
     QPushButton *pushButton;
+    QSpacerItem *horizontalSpacer;
+    QTextBrowser *textBrowser;
 
     void setupUi(QFrame *About)
     {
         if (About->objectName().isEmpty())
             About->setObjectName(QString::fromUtf8("About"));
         About->setWindowModality(Qt::NonModal);
-        About->resize(391, 217);
-        About->setMinimumSize(QSize(387, 202));
+        About->setEnabled(true);
+        About->resize(463, 322);
+        About->setMinimumSize(QSize(0, 0));
+        About->setAutoFillBackground(false);
         About->setFrameShape(QFrame::StyledPanel);
         About->setFrameShadow(QFrame::Raised);
-        textBrowser = new QTextBrowser(About);
-        textBrowser->setObjectName(QString::fromUtf8("textBrowser"));
-        textBrowser->setGeometry(QRect(10, 10, 371, 151));
-        pushButton = new QPushButton(About);
+        gridFrame = new QFrame(About);
+        gridFrame->setObjectName(QString::fromUtf8("gridFrame"));
+        gridFrame->setGeometry(QRect(0, 0, 461, 321));
+        gridLayout = new QGridLayout(gridFrame);
+        gridLayout->setObjectName(QString::fromUtf8("gridLayout"));
+        pushButton = new QPushButton(gridFrame);
         pushButton->setObjectName(QString::fromUtf8("pushButton"));
-        pushButton->setGeometry(QRect(150, 170, 89, 25));
+
+        gridLayout->addWidget(pushButton, 2, 1, 1, 1);
+
+        horizontalSpacer = new QSpacerItem(40, 20, QSizePolicy::Expanding, QSizePolicy::Minimum);
+
+        gridLayout->addItem(horizontalSpacer, 2, 0, 1, 1);
+
+        textBrowser = new QTextBrowser(gridFrame);
+        textBrowser->setObjectName(QString::fromUtf8("textBrowser"));
+
+        gridLayout->addWidget(textBrowser, 1, 0, 1, 2);
+
 
         retranslateUi(About);
-        QObject::connect(pushButton, SIGNAL(clicked()), About, SLOT(close()));
 
         QMetaObject::connectSlotsByName(About);
     } // setupUi
@@ -52,15 +71,13 @@ public:
     void retranslateUi(QFrame *About)
     {
         About->setWindowTitle(QApplication::translate("About", "About", 0, QApplication::UnicodeUTF8));
+        pushButton->setText(QApplication::translate("About", "Close", 0, QApplication::UnicodeUTF8));
         textBrowser->setHtml(QApplication::translate("About", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
 "<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
 "p, li { white-space: pre-wrap; }\n"
 "</style></head><body style=\" font-family:'Sans Serif'; font-size:9pt; font-weight:400; font-style:normal;\">\n"
-"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-family:'DejaVu Sans'; font-size:14pt;\">\320\237\321\200\320\276\320\263\321\200\320\260\320\274\320\274\321\203 \321\200\320\260\320\267\321\200\320\260\320\261\320\276\321\202\320\260\320\273 \321\201\321\202\321\203\320\264\320\265\320\275\321\202 \320\263\321\203\320\277\320\277\321\213 1\320\241I-07 \320\221\320\276\320\275\320\264\320\260\321\200\321\207\321\203\320\272 \320\220\320\273\320\265\320\272\321\201\320\265\320\271 \320\222\320\260\320\273\320\265\321\200\321\214\320\265\320\262\320\270\321\207, \320\275\320\276 \320\276\320"
-                        "\275\320\260 \320\277\320\276\320\272\320\260 \320\265\321\211\320\265 \320\275\320\270\321\207\320\265\320\263\320\276 \320\277\320\276\320\273\320\265\320\267\320\275\320\276\320\263\320\276 \320\275\320\265 \320\264\320\265\320\273\320\260\320\265\321\202.</span></p>\n"
-"<p style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; font-family:'DejaVu Sans'; font-size:14pt;\"></p>\n"
-"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-family:'DejaVu Sans'; font-size:14pt;\">=)</span></p></body></html>", 0, QApplication::UnicodeUTF8));
-        pushButton->setText(QApplication::translate("About", "\320\236\320\272\320\265\321\216\321\210\320\272\320\270", 0, QApplication::UnicodeUTF8));
+"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">\320\237\321\200\320\276\320\263\321\200\320\260\320\274\320\274\321\203 \321\200\320\260\320\267\321\200\320\260\320\261\320\276\321\202\320\260\320\273 \321\201\321\202\321\203\320\264\320\265\320\275 \320\263\321\200\321\203\320\277\321\213 1\320\241\320\206-07</p>\n"
+"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">\320\221\320\276\320\275\320\264\320\260\321\200\321\207\321\203\320\272 \320\220\320\273\320\265\320\272\321\201\320\265\320\271</p></body></html>", 0, QApplication::UnicodeUTF8));
         Q_UNUSED(About);
     } // retranslateUi
 
--- ui_mainwindow.h
+++ ui_mainwindow.h
@@ -1,7 +1,7 @@
 /********************************************************************************
 ** Form generated from reading ui file 'mainwindow.ui'
 **
-** Created: Sun Oct 18 12:54:20 2009
+** Created: Sun Oct 18 19:29:19 2009
 **      by: Qt User Interface Compiler version 4.5.3
 **
 ** WARNING! All changes made in this file will be lost when recompiling ui file!
