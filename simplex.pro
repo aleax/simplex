00c5ccb8d5ddb577b871cb3b4c6945e83019e201
Alexey Bondarchuk <me@aleax.org.ua>
2009-10-18 17:46:56 +0300

    add new files

---
+++ simplex.pro
@@ -0,0 +1,10 @@
+# -------------------------------------------------
+# Project created by QtCreator 2009-09-25T21:18:26
+# -------------------------------------------------
+TARGET = simplex
+TEMPLATE = app
+SOURCES += main.cpp \
+    mainwindow.cpp
+HEADERS += mainwindow.h
+FORMS += mainwindow.ui \
+    about.ui
---
+++ ui_mainwindow.h
@@ -0,0 +1,103 @@
+/********************************************************************************
+** Form generated from reading ui file 'mainwindow.ui'
+**
+** Created: Sun Oct 18 12:54:20 2009
+**      by: Qt User Interface Compiler version 4.5.3
+**
+** WARNING! All changes made in this file will be lost when recompiling ui file!
+********************************************************************************/
+
+#ifndef UI_MAINWINDOW_H
+#define UI_MAINWINDOW_H
+
+#include <QtCore/QVariant>
+#include <QtGui/QAction>
+#include <QtGui/QApplication>
+#include <QtGui/QButtonGroup>
+#include <QtGui/QHeaderView>
+#include <QtGui/QMainWindow>
+#include <QtGui/QMenu>
+#include <QtGui/QMenuBar>
+#include <QtGui/QStatusBar>
+#include <QtGui/QToolBar>
+#include <QtGui/QWidget>
+
+QT_BEGIN_NAMESPACE
+
+class Ui_MainWindow
+{
+public:
+    QAction *actionExit;
+    QAction *actionAbout_Qt;
+    QAction *actionAbout;
+    QWidget *centralWidget;
+    QMenuBar *menuBar;
+    QMenu *menuHello_World;
+    QMenu *menuHelp;
+    QToolBar *mainToolBar;
+    QStatusBar *statusBar;
+
+    void setupUi(QMainWindow *MainWindow)
+    {
+        if (MainWindow->objectName().isEmpty())
+            MainWindow->setObjectName(QString::fromUtf8("MainWindow"));
+        MainWindow->resize(400, 263);
+        actionExit = new QAction(MainWindow);
+        actionExit->setObjectName(QString::fromUtf8("actionExit"));
+        actionExit->setShortcutContext(Qt::ApplicationShortcut);
+        actionAbout_Qt = new QAction(MainWindow);
+        actionAbout_Qt->setObjectName(QString::fromUtf8("actionAbout_Qt"));
+        actionAbout = new QAction(MainWindow);
+        actionAbout->setObjectName(QString::fromUtf8("actionAbout"));
+        centralWidget = new QWidget(MainWindow);
+        centralWidget->setObjectName(QString::fromUtf8("centralWidget"));
+        MainWindow->setCentralWidget(centralWidget);
+        menuBar = new QMenuBar(MainWindow);
+        menuBar->setObjectName(QString::fromUtf8("menuBar"));
+        menuBar->setGeometry(QRect(0, 0, 400, 23));
+        menuHello_World = new QMenu(menuBar);
+        menuHello_World->setObjectName(QString::fromUtf8("menuHello_World"));
+        menuHelp = new QMenu(menuBar);
+        menuHelp->setObjectName(QString::fromUtf8("menuHelp"));
+        MainWindow->setMenuBar(menuBar);
+        mainToolBar = new QToolBar(MainWindow);
+        mainToolBar->setObjectName(QString::fromUtf8("mainToolBar"));
+        MainWindow->addToolBar(Qt::TopToolBarArea, mainToolBar);
+        statusBar = new QStatusBar(MainWindow);
+        statusBar->setObjectName(QString::fromUtf8("statusBar"));
+        MainWindow->setStatusBar(statusBar);
+
+        menuBar->addAction(menuHello_World->menuAction());
+        menuBar->addAction(menuHelp->menuAction());
+        menuHello_World->addAction(actionExit);
+        menuHelp->addAction(actionAbout);
+        menuHelp->addAction(actionAbout_Qt);
+
+        retranslateUi(MainWindow);
+        QObject::connect(actionExit, SIGNAL(activated()), MainWindow, SLOT(close()));
+        QObject::connect(actionAbout_Qt, SIGNAL(activated()), MainWindow, SLOT(aboutQt()));
+        QObject::connect(actionAbout, SIGNAL(activated()), MainWindow, SLOT(about()));
+
+        QMetaObject::connectSlotsByName(MainWindow);
+    } // setupUi
+
+    void retranslateUi(QMainWindow *MainWindow)
+    {
+        MainWindow->setWindowTitle(QApplication::translate("MainWindow", "MainWindow", 0, QApplication::UnicodeUTF8));
+        actionExit->setText(QApplication::translate("MainWindow", "Exit", 0, QApplication::UnicodeUTF8));
+        actionExit->setShortcut(QApplication::translate("MainWindow", "Ctrl+Q", 0, QApplication::UnicodeUTF8));
+        actionAbout_Qt->setText(QApplication::translate("MainWindow", "About Qt", 0, QApplication::UnicodeUTF8));
+        actionAbout->setText(QApplication::translate("MainWindow", "About", 0, QApplication::UnicodeUTF8));
+        menuHello_World->setTitle(QApplication::translate("MainWindow", "File", 0, QApplication::UnicodeUTF8));
+        menuHelp->setTitle(QApplication::translate("MainWindow", "Help", 0, QApplication::UnicodeUTF8));
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class MainWindow: public Ui_MainWindow {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // UI_MAINWINDOW_H
